a28f7318269bb7e362d34830f2e5a882b9bca998
SnakyMz <Soban.syeed@outlook.com>
2023-08-29 23:57:59 +0530

    Add Reservation and cancellation buttons

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,14 +1,17 @@
+import { useDispatch } from 'react-redux';
 import '../styles/Rockets.css';
 import { PropTypes } from 'prop-types';
+import { cancelRocket, reserveRocket } from '../redux/rockets/rocketSlice';
 
 function Rocket({ rocket }) {
+  const dispatch = useDispatch();
   return (
     <div className="rocket" id={rocket.id}>
       <img src={rocket.image} alt="Rocket" className="rocketImg" />
       <div className="rocketDetail">
         <h3 className="rocketHead">{rocket.name}</h3>
         <p className="rocketDesc">{rocket.description}</p>
-        <button type="button" className="reserveRocket">Reserve Rocket</button>
+        {rocket.reserved === false ? <button type="button" className="reserveRocket" onClick={() => dispatch(reserveRocket(rocket.id))}>Reserve Rocket</button> : <button type="button" className="cancelRocket" onClick={() => dispatch(cancelRocket(rocket.id))}>Cancel Reservation</button>}
       </div>
     </div>
   );
