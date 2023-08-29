4e24bfd1a9f56718bb464c86cbf027e0dbeb8212
SnakyMz <Soban.syeed@outlook.com>
2023-08-30 00:27:21 +0530

    Add basic nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <nav />
+    <nav>
+      <NavLink to="/" className="navs">Rockets</NavLink>
+      <NavLink to="/missions" className="navs">Missions</NavLink>
+      <span className="bar">|</span>
+      <NavLink to="/profile" className="navs">My Profile</NavLink>
+    </nav>
   </header>
 );
 
