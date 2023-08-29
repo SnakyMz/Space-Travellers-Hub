0e0f2f7a528439100459a2b760ef45e52fc4b291
SnakyMz <Soban.syeed@outlook.com>
2023-08-30 00:26:47 +0530

    Add my rockets in profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,8 +1,11 @@
 import MyMission from './MyMission';
+import MyRockets from './MyRockets';
+import '../styles/MyProfile.css';
 
 function MyProfile() {
   return (
     <section id="profile">
+      <MyRockets />
       <MyMission />
     </section>
   );
