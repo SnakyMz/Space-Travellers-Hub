d73129cb349202dc7ebf99397f62e538f9465510
SnakyMz <Soban.syeed@outlook.com>
2023-08-29 22:15:37 +0530

    Fetch rockets data using axios

--- src/redux/rockets/rocketSlice.js
+++ src/redux/rockets/rocketSlice.js
@@ -1,29 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit';
-import planet from './planet.png';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const getRockets = createAsyncThunk('rockets/getRockets', async () => {
+  try {
+    const responce = await axios.get('https://api.spacexdata.com/v4/rockets');
+    return responce.data;
+  } catch (error) {
+    return error;
+  }
+});
 
 export const rocketSlice = createSlice({
   name: 'rockets',
-  initialState: [
-    {
-      id: 1,
-      rocket_name: 'Alpha',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam minus est excepturi cumque soluta. Iusto quas nihil asperiores id magnam! Quae numquam doloremque quisquam architecto nam, at in debitis sapiente?',
-      flickr_images: planet,
-    },
-    {
-      id: 2,
-      rocket_name: 'Beta',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam minus est excepturi cumque soluta. Iusto quas nihil asperiores id magnam! Quae numquam doloremque quisquam architecto nam, at in debitis sapiente?',
-      flickr_images: planet,
-    },
-    {
-      id: 3,
-      rocket_name: 'Gamma',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam minus est excepturi cumque soluta. Iusto quas nihil asperiores id magnam! Quae numquam doloremque quisquam architecto nam, at in debitis sapiente?',
-      flickr_images: planet,
-    },
-  ],
+  initialState: {
+    rockets: [],
+    isLoading: false,
+    hasError: false,
+  },
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getRockets.pending, (state) => {
+        state.isLoading = true;
+        state.hasError = false;
+      })
+      .addCase(getRockets.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.hasError = false;
+        state.rockets = action.payload;
+      })
+      .addCase(getRockets.rejected, (state, action) => {
+        state.isLoading = false;
+        state.hasError = action.error;
+      });
+  },
 });
 
 export default rocketSlice.reducer;
