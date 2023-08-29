49ea938ee281ab46538fe60ea4e0459d6f692d57
Asiimwe Mark <markasiimwe75@gmail.com>
2023-08-29 21:27:17 +0300

    Added Nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import { NavLink } from 'react-router-dom';
+import planet from '../images/planet.png';
 
 const Header = () => (
   <header>
-    <nav />
+    <div className="logo">
+      <img src={planet} alt="hub-logo" />
+      <h2>{'Space Traveler\'s Hub'}</h2>
+    </div>
+    <nav>
+      <NavLink className="rocket-link" to="/" activeClassName="active-link">Rockets</NavLink>
+      <NavLink className="mission-link visit" to="/missions" activeClassName="active-link">Missions</NavLink>
+      <div className="divider" />
+      <NavLink className="profile-link" to="/profile" activeClassName="active-link">My Profile</NavLink>
+    </nav>
   </header>
 );
 
