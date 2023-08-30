580a004b3a97e7ef9d4276f7f79cf4ef648b6495
Asiimwe Mark <markasiimwe75@gmail.com>
2023-08-30 22:23:03 +0300

    Added tests for functions and used filter

--- src/components/MyMission.js
+++ src/components/MyMission.js
@@ -2,13 +2,14 @@ import { useSelector } from 'react-redux/es/hooks/useSelector';
 
 const MyMission = () => {
   const missions = useSelector((store) => store.missions.missions);
+  const newMissions = missions.filter((mission) => mission.reserved === true);
   return (
     <section>
       <table>
         <caption><h2>My Missions</h2></caption>
         <tbody>
-          {missions.map((mission) => {
-            if (mission.reserved === true) {
+          {newMissions.map((mission) => {
+            if (mission) {
               return (
                 <tr key={mission.mission_id}>
                   <td><h4>{mission.mission_name}</h4></td>
